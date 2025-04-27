4479e77ff158d65130978c15d33583dc14d68baa
Youssef68Oubenhamou <youssefouben8@gmail.com>
2025-04-27 21:11:16 +0100

    Building a chat system using WebSocket in javascript and node js !

---
+++ index.js
@@ -0,0 +1,56 @@
+const sendBtn = document.getElementById("send-btn");
+const msgInput = document.getElementById("msg-input");
+const msgArea = document.getElementById("msg-area");
+
+const socket = new WebSocket("ws://localhost:8080");
+
+let count = 0;
+
+btnStatus = false;
+
+socket.addEventListener("open" , () => {
+
+    count += 1;
+    console.log("connection established !");
+    console.log("sending to server !");
+    sendBtn.addEventListener("click" , function () {
+
+        btnStatus = true;
+
+        socket.send(msgInput.value);
+
+        msgInput.value = "";
+        
+    });
+    
+});
+
+socket.addEventListener("message" , (e) => {
+
+    let message = e.data;
+    let messageElem = document.createElement("p");
+    messageElem.textContent = message;
+    document.getElementById("msg-space").appendChild(messageElem);
+    console.log(`${e.data}`);
+
+});
+
+socket.addEventListener("close" , (e) => {
+
+    if (e.wasClean) {
+
+        console.log(`Connection was closed cleanly, code=${e.code} reason=${e.reason}`);
+
+    } else {
+
+        console.log("Connection is died");
+
+    }
+
+})
+
+socket.addEventListener("error" , (err) => {
+
+    console.log(`${err}`);
+
+});
---
+++ server.js
@@ -0,0 +1,59 @@
+const WebSocket = require("ws");
+
+// Instantiation the WebSocket Class !
+// Defining the port by passing an object with a key port and it's value
+// to the WebSocket method Server() !
+const server = new WebSocket.WebSocketServer({ port: 8080 });
+
+let connectionStatus;
+
+let clients = new Map();
+
+// Listening on any connection events to] the server using connection event Listener !
+server.on("connection" , function (sock) {
+
+    let username = "User" + Math.floor(Math.random() * 1000);
+
+    clients.set(sock , username);
+
+    connectionStatus = "New Client Connected !";
+
+    console.log(connectionStatus);
+
+    // Listening on any message events using message event Listener !
+    sock.on("message" , function (msg) {
+
+        msg = `${clients.get(sock)} : ` + msg;
+
+        // Showing the data !
+        console.log(`The data is received : ${msg}`);
+
+        let i = 0;
+        
+        // Looping over the connected clients using forEach !
+        server.clients.forEach((client) => {
+
+            // If the state of client is connected or the WebSocket is OPEN 
+            if (client.readyState === WebSocket.OPEN) {
+
+                i += 1;
+
+                // Send the message to the client
+                client.send(`${msg}`);
+
+            }
+            
+        });
+
+    })
+
+    // Closing the socket when the client is disconnected !
+    sock.addEventListener("close" , function () {
+
+        console.log("Client Disconnected !");
+
+    });
+
+})
+
+console.log("WebSocket server running on ws://localhost:8080");
